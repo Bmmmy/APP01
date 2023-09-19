297b5b9f172e52b38942afd57a38cebe3adde105
Bmmmy <zxc1220amy@gmail.com>
2023-09-19 22:01:41 +0800

    "新增註冊功能"-B-9/19 詳細備註: 新增"註冊"到"註冊登入APP"。 尚未檢查註冊資料已存在等問題，單純將資料導入資料庫。

---
+++ app/src/main/java/com/example/app01/DBHelper.java
@@ -0,0 +1,78 @@
+package com.example.app01;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper{
+    //版本
+    private final static int DBVersion = 1;
+    private final static String DBName = "CameraDB.db";
+    private final static String TableName = "CameraDB";
+
+    public DBHelper(Context context, String name, CursorFactory factory, int version){
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // 程式首次運行，且資料庫不存在時(第一次載入)，會自動生成id,Account,Password的資料表!
+        String SQLTable = "CREATE TABLE IF NOT EXISTS " + TableName + "(" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "Account TEXT, " +
+                "Password TEXT, " +
+                "CheckPassword TEXT" +
+                ");";
+        db.execSQL(SQLTable);
+    }
+
+    //onUpgrade暫時不知道要升級時如何操作
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // 資料庫結構有改變了就會用到onUpgrade ex.1.改版本2.改資料庫結構(add表...)3.刪除舊的表,then移到新的
+        //DROP TABLE 刪除舊的TableName表，then我可以再創一個新的
+        final String SQL = "DROP TABLE " + TableName;
+        sqLiteDatabase.execSQL(SQL);
+    }
+
+    //檢查table有無正常被創建 若沒有就創建
+    public void checkDBTable() {
+        //Cursor android中查詢資料的介面，他會收到一個結果集
+        //getWriteableDatabases() --> 它返回一個可寫入的資料庫物件。 這裡是把抓到的TableName寫進去
+        //利用sql語言來檢查資料庫中的所有資料表，是否存在有那個名字的表 (where ... ,條件:TableName)
+        //sqlite_master 是 SQLite的內建系統表，不動他。
+        Cursor cursor = getWritableDatabase().rawQuery(
+                "SELECT DISTINCT tbl_name FROM sqlite_master WHERE tbl_name = '" +
+                        TableName + "';", null);
+
+        //cursor == null 代表sql查詢錯誤
+        //!=null來確保cursor正確初始化
+        if (cursor != null) {
+            if (cursor.getCount() == 0) {
+                getWritableDatabase().execSQL("CREATE TABLE IF NOT EXISTS " + TableName + "(" +
+                        "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        "Account TEXT, " +
+                        "Password TEXT, " +
+                        "CheckPassword TEXT" +
+                        ");");
+                cursor.close();
+            }
+        }
+    }
+
+    public boolean registerFunction(String account, String password, String checkPd){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("Account", account);
+        values.put("Password", password);
+        values.put("CheckPassword", checkPd);
+        long result = db.insert(TableName, null, values);
+
+        //若插入成功的話 return ture (ture = 1) SQLite錯誤會直接回傳-1;
+        return result != -1;
+    }
+
+}
--- app/src/main/java/com/example/app01/RegisterActivity.java
+++ app/src/main/java/com/example/app01/RegisterActivity.java
@@ -1,18 +1,31 @@
 package com.example.app01;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class RegisterActivity extends Activity {
+
+    //註冊功能所需
+    private EditText accountEditText, passwordEditText,checkPdEditText;
+    private Button registerButton;
+    //顯示消息設定"showToast"
+    private void showToast(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //返回按鍵
         Button backButton = findViewById(R.id.button4);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +35,51 @@ public class RegisterActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        //註冊按鍵
+        // 創建 DBHelper 實例
+        DBHelper dbHelper = new DBHelper(this, "CameraDB", null, 1);
+
+        // 檢查資料庫是否存在，如果不存在則創建
+        dbHelper.checkDBTable();
+
+        // 找到 UI 元素
+        accountEditText = findViewById(R.id.editTextText);
+        passwordEditText = findViewById(R.id.editTextText4);
+        checkPdEditText = findViewById(R.id.editTextText5);
+
+
+        registerButton = findViewById(R.id.button3);
+
+
+        // 設定新增按鈕的點擊事件處理程式
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 讀取使用者輸入的資料
+                String account = accountEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String checkPd = checkPdEditText.getText().toString();
+
+                // 將資料新增到資料庫
+                boolean checkRegister = dbHelper.registerFunction(account, password, checkPd);
+
+                if(checkRegister){
+                    //show成功字樣
+                    showToast("新增成功");
+                    // 清空輸入欄位
+                    accountEditText.setText("");
+                    passwordEditText.setText("");
+                    checkPdEditText.setText("");
+
+                }else{
+                    //show 失敗字樣
+                    showToast("新增失敗，請重新輸入!");
+                }
+
+            }
+        });
+
     }
 }
